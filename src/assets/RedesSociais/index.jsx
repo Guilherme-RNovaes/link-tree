a3fefc67827e654ecf90a7eace2de6c8b8f3b185
guilh <guilherme00rogerio@gmail.com>
2023-04-12 20:43:40 -0300

    Criação do primeiro componente para redes sociais

---
+++ src/assets/RedesSociais/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function RedesSociais() {
+  const ListaRedes = [
+    { id: 1, urlNome: 'Github', url: 'https://github.com/Guilherme-RNovaes' }
+    { id: 2, urlNome: 'LinkedIn', url: 'https://www.linkedin.com/in/guilherme-r-novaes/' }
+    { id: 3, urlNome: 'Instagram', url: 'https://www.instagram.com/gr.novaes/' }
+  ]
+  return (
+    <>
+    </>
+  )
+}
+
+export default RedesSociais
