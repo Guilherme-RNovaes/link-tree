62276ae280fd787e8b366a8e6ac107103f7389de
guilh <guilherme00rogerio@gmail.com>
2023-04-13 20:54:58 -0300

    Finalizando o Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import './index.css'
+import Header from './components/Header'
 import RedesSociais from './components/RedesSociais'
+
+import './index.css'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <RedesSociais />
     </div>
   )
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+function Header() {
+  return (
+    <div className="perfil-content">
+        <div className="perfil-img"></div>
+        <h3>Guilherme Novaes</h3>
+        <p>Bem Vindo!</p>
+    </div>
+  )
+}
+
+export default Header
