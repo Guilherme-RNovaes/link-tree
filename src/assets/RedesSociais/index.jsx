222701f5abce5177208756e6f700b77766d19f42
guilh <guilherme00rogerio@gmail.com>
2023-04-13 18:10:55 -0300

    modificação de algumas redes

--- src/assets/RedesSociais/index.jsx
+++ src/assets/RedesSociais/index.jsx
@@ -5,7 +5,8 @@ function RedesSociais() {
   const ListaRedes = [
     { id: 1, urlName: 'Github', url: 'https://github.com/Guilherme-RNovaes' },
     { id: 2, urlName: 'LinkedIn', url: 'https://www.linkedin.com/in/guilherme-r-novaes/' },
-    { id: 3, urlName: 'Instagram', url: 'https://www.instagram.com/gr.novaes/' }
+    { id: 3, urlName: 'Instagram', url: 'https://www.instagram.com/gr.novaes/' },
+    { id: 4, urlName: 'Portifólio', url: 'https://guilherme-novaes-portifolio.vercel.app/'}
   ]
 
   const LinksRedes = ListaRedes.map((props) =>
